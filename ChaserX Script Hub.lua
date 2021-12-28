399e2ea90edfc9738f2589d6420d323d56ff1187
ChaseOSDev <devChase0842@outlook.com>
2021-12-28 14:42:41 -0500

    new script

--- ChaserX Script Hub.lua
+++ ChaserX Script Hub.lua
@@ -2,7 +2,7 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 local Window = Library.CreateLib("well543211's Script Hub", "DarkTheme")
 local GeneralScripts = Window:NewTab("General Scripts")
 local Scripts = GeneralScripts:NewSection("Scripts")
-local ReanimationScripts = Window:NewTab("Reanimation/Hat Scripts")
+local ReanimationScripts = Window:NewTab("Animation Scripts")
 local ReanimationScriptSection = ReanimationScripts:NewSection("Scripts")
 Scripts:NewButton("Fates ESP & Aimbot", "Cool ESP.", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/fatesc/fates-esp/main/main.lua'))()
@@ -10,7 +10,17 @@ end)
 Scripts:NewButton("Reviz Admin", "Admin that is good for any game usually.", function()
 	loadstring(game:HttpGet("https://pastebin.com/raw/Caniwq2N", true))()
 end)
-
 ReanimationScriptSection:NewButton("Drone Animation (Not FE)", "Something with a drone.", function()
 	loadstring(game:HttpGet("https://pastebin.com/raw/k5eWfDYa", true))()
+end)
+local PlayerTab = Window:NewTab("Player")
+local PlayerSettings = PlayerTab:NewSection("PlayerSettings")
+PlayerSettings:NewSlider("Speed", "Your walk speed.", 64, 0, function(a)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = a
+end)
+PlayerSettings:NewSlider("Jump Power", "Your jump power.", 100, 0, function(a)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = a
+end)
+PlayerSettings:NewToggle("Sit", "If you are sitting or not.", function(a)
+	game.Players.LocalPlayer.Character.Humanoid.Sit = a
 end)
