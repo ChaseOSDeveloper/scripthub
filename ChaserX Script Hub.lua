61bed8d810f3a7a236c7c401a309735b362ca21a
ChaseOSDev <devChase0842@outlook.com>
2021-12-29 11:08:32 -0500

    a

--- ChaserX Script Hub.lua
+++ ChaserX Script Hub.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("well543211's Script Hub", "DarkTheme")
+local Window = Library.CreateLib("well543211's Script Hub", "Synapse")
 local GeneralScripts = Window:NewTab("General Scripts")
 local Scripts = GeneralScripts:NewSection("Scripts")
 local ReanimationScripts = Window:NewTab("Animation Scripts")
